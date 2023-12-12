9f35175ddadc04588d5cbe9f29461f27123a92b6
Dmitrii Dolmatov <mr.dolmatov-2001@mail.ru>
2023-12-12 22:56:49 +0300

    Initial commit

---
+++ src/test/java/ru/dolmatov/weathersensorapi/WeatherSensorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolmatov.weathersensorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
